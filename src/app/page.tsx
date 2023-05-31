8f2d8bf9b2ee467877d7a4552f51683ae47df3b0
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-31 13:54:27 +0530

    Create different variants for button component, using class variance authority

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,26 @@ export default function Home() {
   return (
     <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-3">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+        <h1 className="font-extrabold text-3xl tracking-tighter leading-tight sm:text-3xl md:text-5xl lg:text-6xl">
           Simplify Image Optimization with <br className="hidden sm:inline" />
           OptimizeX
         </h1>
-        <p className="max-w-[700px] text-lg text-zinc-700 dark:text-zinc-400 sm:text-xl">
+        <p className="text-zinc-700 dark:text-zinc-400 max-w-[700px] text-lg sm:text-xl">
           Effortlessly bulk-generate optimized versions of your images with our
           free and open-source tool.
         </p>
       </div>
-      <div className="flex gap-4"></div>
-      <div className="flex flex-col gap-4 mt-12"></div>
+      <div className="flex gap-4">
+        {/**
+         * Upload Section
+         */}
+      </div>
+      <div className="flex flex-col gap-4 mt-12">
+        <p className=" text-sm text-zinc-400">
+          All images are used solely for alt-generation and are automatically
+          deleted after 24h.
+        </p>
+      </div>
     </section>
   );
 }
---
+++ src/components/Input/Button.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Link from "next/link";
+import { VariantProps, cva } from "class-variance-authority";
+
+import mergeClasses from "@/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 dark:hover:bg-zinc-800 dark:hover:text-zinc-100 disabled:opacity-50 dark:focus:ring-zinc-400 disabled:pointer-events-none dark:focus:ring-offset-zinc-900 data-[state=open]:bg-zinc-100 dark:data-[state=open]:bg-zinc-800",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-zinc-900 text-white hover:bg-zinc-700 dark:bg-zinc-50 dark:text-zinc-900",
+        destructive:
+          "bg-zinc-500 text-white hover:bg-zinc-600 dark:hover:bg-zinc-600",
+        outline:
+          "bg-transparent border border-zinc-200 hover:bg-zinc-100 dark:border-zinc-700 dark:text-zinc-100",
+        subtle:
+          "bg-zinc-100 text-zinc-900 hover:bg-zinc-200 dark:bg-zinc-700 dark:text-zinc-100",
+        ghost:
+          "bg-transparent dark:bg-transparent hover:bg-zinc-100 dark:hover:bg-zinc-800 dark:text-zinc-100 dark:hover:text-zinc-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
+        link: "bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-zinc-900 dark:text-zinc-300 hover:bg-transparent dark:hover:bg-transparent",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2 rounded-md",
+        lg: "h-11 px-8 rounded-md",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
