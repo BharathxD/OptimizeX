c1de871b5c4ffef05c0d952cc2f928b7701d44d5
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-06-01 17:17:41 +0530

    Implement Singleton Prisma Client

---
+++ src/libs/prismadb.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+// Create a new Prisma client if it doesn't exist already
+const client = global.prisma || new PrismaClient();
+
+/**
+ * Initialize the Prisma client.
+ * 
+ * If the `prisma` global variable is not defined, create a new PrismaClient instance.
+ * This is useful for maintaining a single instance of the Prisma client across the application.
+ * 
+ * If the environment is not in production, assign the Prisma client to the `prisma` global variable.
+ * This is done to support NextJS Fast-Refresh, which may create multiple instances of the Prisma client during development.
+ * By resetting the `prisma` global variable on each module reload, we ensure that the latest Prisma client instance is used.
+ * 
+ * Note: `global` is a constant reference to the global object, and hot reloading only affects the code modules that are being updated, not the global object itself.
+ */
+if (process.env.NODE_ENV !== "production") global.prisma = client;
+
+export default client;
