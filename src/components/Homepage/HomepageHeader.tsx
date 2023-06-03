056c216cf9a9385ec726718607629441812f88cf
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-06-04 00:14:51 +0530

    Added loading skeleton for the homepage image

--- src/components/Homepage/HomepageHeader.tsx
+++ src/components/Homepage/HomepageHeader.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Fragment } from "react";
 
 const HomepageHeader = () => {
--- src/components/Homepage/HomepageImage.tsx
+++ src/components/Homepage/HomepageImage.tsx
@@ -1,16 +1,28 @@
+"use client";
+
 import Image from "next/image";
 
 const HomepageImage = () => {
+  const loadingSkeleton = (
+    <div
+      role="status"
+      className="mt-4 h-[59rem] w-[54rem] bg-zinc-700 hidden animate-pulse md:block aspect-video md:-ml-7 rounded-lg -mr-10"
+    ></div>
+  );
+
   return (
-    <Image
-      src="/images/homepage.png"
-      alt="Homepage Image"
-      width={100}
-      unoptimized={true}
-      height={100}
-      priority={true}
-      className="hidden md:block aspect-video w-full md:w-[110vh] md:-ml-7 rounded-lg -mr-10"
-    />
+    <div>
+      <Image
+        src="/images/homepage.png"
+        alt="Homepage Image"
+        width={100}
+        unoptimized={true}
+        height={100}
+        priority={true}
+        onLoad={() => loadingSkeleton}
+        className="hidden md:block aspect-video w-full md:w-[110vh] rounded-lg"
+      />
+    </div>
   );
 };
 
