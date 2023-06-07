a28a90e84f0c882cf8b401b39ce92081f0f9abbe
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-06-07 15:31:26 +0530

    Fix: Use date-fns-tz

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,40 +1,33 @@
+import { format, utcToZonedTime } from 'date-fns-tz';
+
 const enum TimeAgo {
     Today = 'Today',
     Yesterday = 'Yesterday',
-    TwoDaysAgo = "Two days ago",
-    MoreThanTwoDaysAgo = 'More than two days ago'
+    TwoDaysAgo = 'Two days ago',
+    MoreThanTwoDaysAgo = 'More than two days ago',
 }
 
 const formatDate = (targetDate: Date): string => {
     const currentDate = new Date();
-    // Current Time Zone Offset
-    const currentTimezoneOffset = currentDate.getTimezoneOffset();
-    // IST offset in minutes
-    const ISTOffset = 330;
-    const serverOffset = currentTimezoneOffset + ISTOffset * 60 * 1000;
-    const serverTime = currentDate.getTime() + serverOffset;
-    const serverDate = new Date(serverTime);
+    const serverTime = utcToZonedTime(currentDate, 'IST');
+    const targetTime = utcToZonedTime(targetDate, 'Asia/Kolkata');
 
-    const timeDifference = serverDate.getTime() - targetDate.getTime();
+    const timeDifference = serverTime.getTime() - targetTime.getTime();
     const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
 
     if (timeDifference < oneDayInMilliseconds) {
-        return `${TimeAgo.Today} at ${formatTime(targetDate)}`;
-    } else if (timeDifference > oneDayInMilliseconds && timeDifference < 2 * oneDayInMilliseconds) {
-        return `${TimeAgo.Yesterday} at ${formatTime(targetDate)}`;
-    } else if (timeDifference > 2 * oneDayInMilliseconds && timeDifference < 3 * oneDayInMilliseconds) {
-        return `${TimeAgo.TwoDaysAgo} at ${formatTime(targetDate)}`;
+        return `${TimeAgo.Today} at ${formatTime(targetTime)}`;
+    } else if (timeDifference < 2 * oneDayInMilliseconds) {
+        return `${TimeAgo.Yesterday} at ${formatTime(targetTime)}`;
+    } else if (timeDifference < 3 * oneDayInMilliseconds) {
+        return `${TimeAgo.TwoDaysAgo} at ${formatTime(targetTime)}`;
     } else {
         return TimeAgo.MoreThanTwoDaysAgo;
     }
-}
+};
 
 const formatTime = (date: Date): string => {
-    return date.toLocaleString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-    });
-}
+    return format(date, 'h:mm a', { timeZone: 'Asia/Kolkata' });
+};
 
 export default formatDate;
