130417a9aabc53556e844b1c7b55daed2ee5b55c
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-06-07 16:52:37 +0530

    Fix: Open Graph meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,20 +7,29 @@ import LoginModal from "@/components/Modals/LoginModal";
 import RtkProvider from "@/providers/RtkProvider";
 import RegisterModal from "@/components/Modals/RegisterModal";
 import getCurrentUser from "@/actions/getCurrentUser";
-import { Toaster } from "react-hot-toast";
 import ToasterProvider from "@/providers/ToasterProvider";
-import { usePathname } from "next/navigation";
 import Head from "next/head";
+import { Metadata } from "next";
 
 const font = Montserrat({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "OptimizeX",
   description:
     "Effortlessly bulk-generate optimized versions of your images with our free and open-source tool.",
+  openGraph: {
+    title: "OptimizeX",
+    description:
+      "Effortlessly bulk-generate optimized versions of your images with our free and open-source tool.",
+    images:
+      "https://media-bucket-project.s3.ap-south-1.amazonaws.com/og-render.png",
+    url: "https://optimizex.vercel.app/",
+    type: "website",
+    siteName: "OptimizeX",
+  },
 };
 
 // Anti-aliazing - Improving the edges of the font
@@ -39,20 +48,6 @@ export default async function RootLayout({
         font.variable
       )}
     >
-      <Head>
-        <meta property="og:title" content="OptimizeX" />
-        <meta
-          property="og:description"
-          content="Effortlessly bulk-generate optimized versions of your images with our free and open-source tool."
-        />
-        <meta
-          property="og:image"
-          content="https://media-bucket-project.s3.ap-south-1.amazonaws.com/og-render.png"
-        />
-        <meta property="og:url" content="https://optimizex.vercel.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="OptimizeX" />
-      </Head>
       <body
         className={`min-h-screen font-sans antialiased bg-zinc-950 text-zinc-50`}
         suppressHydrationWarning={true}
