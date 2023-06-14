6545406369c3df7a633b13172d0531c68ed084f7
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-06-14 19:46:03 +0530

    Refactor: optimize routes and pollOptimizedImagesFromS3

--- src/app/api/optimize/route.ts
+++ src/app/api/optimize/route.ts
@@ -1,61 +1,27 @@
 import { StatusCodes } from "http-status-codes";
 import { NextRequest, NextResponse } from "next/server";
-import s3 from "../../../../aws/s3";
 import getCurrentUser from "@/actions/getCurrentUser";
-
-const MAX_RETRIES = 5;
-const RETRY_DELAY = 5000;
-
-async function pollOptimizedImageFromS3(key: string, fileMetadata: { name: string, type: string }, retries = MAX_RETRIES): Promise<NextResponse> {
-    try {
-        const getObjectParams = {
-            Bucket: process.env.NEXT_AWS_S3_DESTINATION_BUCKET_NAME!,
-            Key: key,
-        };
-        const response = await s3.getObject(getObjectParams).promise();
-
-        if (response.Body) {
-            return new NextResponse(response.Body as ArrayBuffer, {
-                status: StatusCodes.OK,
-                headers: {
-                    "Content-Type": fileMetadata.type,
-                    "Content-Disposition": `attachment; filename=${fileMetadata.name}`,
-                },
-            });
-        } else {
-            return new NextResponse(null, { status: StatusCodes.NOT_FOUND });
-        }
-    } catch (error: any) {
-        if (error.code === "NoSuchKey") {
-            if (retries > 0) {
-                await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
-                return pollOptimizedImageFromS3(key, fileMetadata, retries - 1);
-            } else {
-                return new NextResponse(
-                    JSON.stringify({
-                        message:
-                            "Sorry, it's taking too long to process. You can check your profile later to download it.",
-                    }),
-                    { status: StatusCodes.NOT_FOUND }
-                );
-            }
-        } else {
-            console.error(error);
-            return new NextResponse(null, {
-                status: StatusCodes.INTERNAL_SERVER_ERROR,
-            });
-        }
-    }
-}
+import pollOptimizedImageFromS3 from "@/handlers/pollOptimizedImageFromS3";
+import prisma from "@/libs/prismadb";
 
 export async function GET(req: NextRequest) {
     try {
         const currentUser = await getCurrentUser();
         if (!currentUser) {
-            return NextResponse.json({ message: "User is not authenticated to perform this operation" }, { status: StatusCodes.UNAUTHORIZED });
+            return NextResponse.json({ message: "User is not authenticated to perform this operation." }, { status: StatusCodes.UNAUTHORIZED });
         }
         const { key } = await req.json();
-
+        const isAuthorized = await prisma.optimizedImage.findFirst({
+            where: {
+                objectKey: key,
+                userId: currentUser.id
+            },
+        })
+        if (!isAuthorized) {
+            return NextResponse.json({ message: "User is not authorized to perform this operation." })
+        }
+        // TODO: Fetch Image from S3
+        // return new NextResponse(foundImageBuffer as Buffer, { status: StatusCodes.OK });
     } catch (error: any) {
         return NextResponse.json({ message: "Internal Server Error" }, { status: StatusCodes.INTERNAL_SERVER_ERROR });
     }
@@ -65,15 +31,18 @@ export async function POST(req: NextRequest) {
     try {
         const currentUser = await getCurrentUser();
         if (!currentUser) {
-            return NextResponse.json({ message: "User is not authenticated to perform this operation" }, { status: StatusCodes.UNAUTHORIZED });
+            return NextResponse.json({ message: "User is not authenticated to perform this operation." }, { status: StatusCodes.UNAUTHORIZED });
         }
         const { key, file } = await req.json();
-        const response = await pollOptimizedImageFromS3(key, file, 10);
-        return response;
-    } catch (error: any) {
-        console.error(error);
-        return new NextResponse(null, {
-            status: StatusCodes.INTERNAL_SERVER_ERROR,
+        const responseBuffer: Buffer | null = await pollOptimizedImageFromS3(key, file);
+        if (!responseBuffer) {
+            return NextResponse.json({ message: "Sorry, it's taking too long to process. You can check your profile later to download it." }, { status: StatusCodes.NOT_FOUND });
+        }
+        return new NextResponse(responseBuffer, {
+            status: StatusCodes.OK,
+            headers: { "Content-Type": file.type, "Content-Disposition": `attachment; filename=${file.name}` },
         });
+    } catch (error) {
+        return new NextResponse(null, { status: StatusCodes.INTERNAL_SERVER_ERROR });
     }
 }
--- src/handlers/pollOptimizedImageFromS3.ts
+++ src/handlers/pollOptimizedImageFromS3.ts
@@ -1,50 +1,26 @@
-import { NextResponse } from "next/server";
 import s3 from "../../aws/s3";
-import { StatusCodes } from "http-status-codes";
 
 const MAX_RETRIES = 5;
 const RETRY_DELAY = 5000;
 
-const pollOptimizedImageFromS3 = async (key: string, fileMetadata: { name: string, type: string }, retries = MAX_RETRIES): Promise<NextResponse> => {
+const pollOptimizedImageFromS3 = async (
+    key: string,
+    fileMetadata: { name: string; type: string },
+    retries = MAX_RETRIES,
+): Promise<Buffer | null> => {
     try {
-        const getObjectParams = {
-            Bucket: process.env.NEXT_AWS_S3_DESTINATION_BUCKET_NAME!,
-            Key: key,
-        };
+        const getObjectParams = { Bucket: process.env.NEXT_AWS_S3_DESTINATION_BUCKET_NAME!, Key: key };
         const response = await s3.getObject(getObjectParams).promise();
-
-        if (response.Body) {
-            return new NextResponse(response.Body as ArrayBuffer, {
-                status: StatusCodes.OK,
-                headers: {
-                    "Content-Type": fileMetadata.type,
-                    "Content-Disposition": `attachment; filename=${fileMetadata.name}`,
-                },
-            });
-        } else {
-            return new NextResponse(null, { status: StatusCodes.NOT_FOUND });
-        }
+        if (response.Body instanceof Buffer) return response.Body;
+        return null;
     } catch (error: any) {
         if (error.code === "NoSuchKey") {
-            if (retries > 0) {
-                await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
-                return pollOptimizedImageFromS3(key, fileMetadata, retries - 1);
-            } else {
-                return new NextResponse(
-                    JSON.stringify({
-                        message:
-                            "Sorry, it's taking too long to process. You can check your profile later to download it.",
-                    }),
-                    { status: StatusCodes.NOT_FOUND }
-                );
-            }
-        } else {
-            console.error(error);
-            return new NextResponse(null, {
-                status: StatusCodes.INTERNAL_SERVER_ERROR,
-            });
+            if (!(retries > 0)) return null;
+            await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+            return pollOptimizedImageFromS3(key, fileMetadata, retries - 1);
         }
+        return null;
     }
-}
+};
 
 export default pollOptimizedImageFromS3;
