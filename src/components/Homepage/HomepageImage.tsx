6d3a0652873a485a2402d36c9a512254f6d49faa
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-06-15 21:36:08 +0530

    Made landing page image render more pleasant

--- src/components/Homepage/HomepageImage.tsx
+++ src/components/Homepage/HomepageImage.tsx
@@ -1,17 +1,24 @@
+"use client";
+
 import Image from "next/image";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 const HomepageImage = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const handleImageLoad = () => setIsLoaded(true);
   return (
     <Fragment>
       <Image
-        src="/images/homepage.webp"
+        src="https://media-bucket-project.s3.ap-south-1.amazonaws.com/homepage.webp"
         alt="Homepage Image"
         width={100}
         priority={true}
         unoptimized={true}
+        onLoad={handleImageLoad}
         height={100}
-        className={`hidden md:block aspect-video w-[110vh] rounded-lg`}
+        className={`hidden md:block aspect-video w-[110vh] rounded-lg ${
+          !isLoaded ? "opacity-0" : "opacity-100"
+        } transition-opacity`}
       />
     </Fragment>
   );
