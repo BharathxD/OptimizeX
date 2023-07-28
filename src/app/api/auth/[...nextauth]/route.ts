66ce0ffad1b874e6f18a81200edefcaeee3b434b
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-07-28 20:33:09 +0530

    chore: add documentation to next-auth route handler

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,13 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
+
 import { authOptions } from "@/libs/auth";
 
+/**
+ * Initialize the NextAuth.js authentication handler with the provided options.
+ * @type {import("next-auth").NextApiHandler}
+ * @see {@link https://next-auth.js.org/getting-started/introduction}
+ * @see {@link https://next-auth.js.org/configuration/options}
+ */
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
