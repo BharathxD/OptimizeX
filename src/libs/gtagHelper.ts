e7824ad99cda0beb5618a13a577bf5263d30d6d6
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-08-08 20:32:53 +0530

    chore: refactor gtag Utility function

--- src/libs/gtagHelper.ts
+++ src/libs/gtagHelper.ts
@@ -1,5 +1,12 @@
-export const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
+/**
+ * Sends a pageview event to Google Analytics.
+ * @param GA_MEASUREMENT_ID - The Google Analytics measurement ID.
+ * @param url - The URL of the page.
+ */
+const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
   window.gtag("config", GA_MEASUREMENT_ID, {
     page_path: url,
   });
 };
+
+export { pageview };
