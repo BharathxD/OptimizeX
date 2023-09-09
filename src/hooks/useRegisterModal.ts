2b159b7a6d4dc040249de7137a71ee120a152815
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-09-09 22:02:23 +0530

    chore: improve code documentation

--- src/hooks/useRegisterModal.ts
+++ src/hooks/useRegisterModal.ts
@@ -6,6 +6,11 @@ interface useRegisterModalStore {
   onClose: () => void;
 }
 
+/**
+ * Custom hook for managing the state of a Register Modal.
+ * @param {function} set - A function provided by Zustand to update the state.
+ * @returns {useRegisterModalStore} The state and actions for the Register Modal.
+ */
 const useRegisterModal = create<useRegisterModalStore>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
